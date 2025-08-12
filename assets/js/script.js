4fe99750845120a2794bdc9e488530360a1b6483
vinicius inocêncio <134207832+inocenciooo@users.noreply.github.com>
2025-08-12 20:51:43 -0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const menuButton = document.querySelector(".menu-button");
+const menuList = document.querySelector("#menu");
+
+
+
+menuButton.addEventListener("click",()=>{
+    menuList.classList.toggle("active")
+    menuButton.classList.toggle("active")
+})
