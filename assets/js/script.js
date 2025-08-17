f1e2e952d4db28e7f36949303915ad387edbe38c
vinicius inocêncio <vinicius1silva61@gmail.com>
2025-08-17 18:18:36 -0300

    estilo formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
 const menuButton = document.querySelector(".menu-button");
 const menuList = document.querySelector("#menu");
+const form = document.querySelector("form");
+const formInputs = document.querySelectorAll(".input");
+const textoErro = form.querySelectorAll("p");
 
+menuButton.addEventListener("click", () => {
+  menuList.classList.toggle("active");
+  menuButton.classList.toggle("active");
+});
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  inputErro();
+});
 
-menuButton.addEventListener("click",()=>{
-    menuList.classList.toggle("active")
-    menuButton.classList.toggle("active")
-})
+function inputErro() {
+  formInputs.forEach((input) => {
+    if (input.value === "") {
+      input.parentElement.classList.add("mensagem-erro");
+    } else {
+        input.parentElement.classList.remove("mensagem-erro");
+    }
+  });
+}
